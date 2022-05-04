5357904684f42c067844fe97ea4183336cc79346
Rafael Hernandez <Rafael.Hernandez852@gmail.com>
2022-05-03 23:56:32 -0700

    minor update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   var [pages,setPage] = useState();
   
   useEffect(()=>{
-    fetch("/index").then((res)=>res.json())
+    fetch("/Blog/index").then((res)=>res.json())
       .then((data)=> setPage(data.names));
   },[]);
 
