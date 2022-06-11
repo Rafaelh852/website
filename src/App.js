f4610bb47290f3a8d8db5ea159d5e477eeb1175e
Rafael Hernandez <Rafael.Hernandez852@gmail.com>
2022-06-10 17:57:05 -0700

    UA tags

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, {useEffect,useState} from "react";
 import Page from "./components/Page";
 import {BrowserRouter} from "react-router-dom";
 import Routes from "./Routes";
+import ReactGA from "react-ga";
 
+const TRACKING_ID = "UA-231703413-1";
 
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  
+
   // Uninitialized state will cause Child to error out
   var [pages,setPage] = useState();
-  
   useEffect(()=>{
     fetch("/blog/index").then((res)=>res.json())
       .then((data)=> setPage(data.names));
   },[]);
+
+useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    console.log(ReactGA);
+  }, []);
+
+  
 
   // don't render Child until `pages` is ready!
   return (
